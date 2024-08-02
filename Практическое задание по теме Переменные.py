a0d555ee7e890e5dd40057eec3112c3c2251af89
dtalovera <48647274+dtalovera@users.noreply.github.com>
2024-08-02 21:09:24 +0300

    Add files via upload

---
+++ Практическое задание по теме Переменные.py
@@ -0,0 +1,6 @@
+amount_of_homework = 12
+amount_of_hours_spent = 1.5
+course_title = "Python"
+time_for_one_task = amount_of_hours_spent / amount_of_homework
+
+print("Курс: " + course_title + ", всего задач:" + str(amount_of_homework), ", затрачено часов: " + str(amount_of_hours_spent) + ", среднее время выполнения " + str(time_for_one_task) + " часа.")
